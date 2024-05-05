bcaac9580c4903e711c51383f9596171b02624f2
isold <isold.wang@qq.com>
2024-05-05 19:15:04 +0800

    TMovie

--- app/src/main/java/com/melon/tmovie/MainActivity.java
+++ app/src/main/java/com/melon/tmovie/MainActivity.java
@@ -1,10 +1,13 @@
 package com.melon.tmovie;
 
+
 import android.os.Build;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.webkit.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Collections;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +16,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView webView = findViewById(R.id.webview);
+
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient(){
-           @Override
-           public boolean shouldOverrideUrlLoading(WebView view, String url) {
-               // 返回true代表链接会在WebView中打开，而不是使用默认浏览器
-               view.loadUrl(url);
-               return true;
-           }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            webView.getSettings().setSafeBrowsingEnabled(false);
+        }
+
+        webView.getSettings().setAllowFileAccess(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        }
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.getSettings().setAllowFileAccess(true);
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                WebResourceResponse response = super.shouldInterceptRequest(view, request);
+                if (response != null) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        response.setResponseHeaders(Collections.singletonMap("Access-Control-Allow-Origin", "*"));
+                    }
+                }
+                return response;
+            }
         });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                //Log.d("WebView", consoleMessage.message());
+                return true;
+            }
+        });
+
         webView.loadUrl("http://tv.hzdianyue.com");
     }
 }
